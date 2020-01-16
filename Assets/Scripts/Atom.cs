a5e679f3328a25096d2821b3da203f48df09e112
Horácio Macêdo <horacio97@gmail.com>
2020-01-16 13:59:49 -0300

    hello world
    First commit, putting out there what I've already done.
    Right now, I can merge two meshes when two markers are tracked. It lacks any polish, and will merge the meshes when tracked withou any regard for relative positions nor any of this.
    
    Top priority is to make the merge happen only when close from one another.

---
+++ Assets/Scripts/Atom.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using System.Linq;
+using Vuforia;
+
+public class Atom : MonoBehaviour {
+
+    public string Name;
+    public int Valence;
+    public TrackableBehaviour self;
+
+    public GameManager gm;
+
+    public Atom(string Name, int Valence, TrackableBehaviour Self){
+        this.Name = Name;
+        this.Valence = Valence;
+        this.self = Self;
+    }
+
+    void Start(){
+        GameObject model = this.gameObject.transform.GetChild(0).gameObject;
+        if (!model.activeSelf){
+            model.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (self.CurrentStatus == TrackableBehaviour.Status.TRACKED || self.CurrentStatus == TrackableBehaviour.Status.EXTENDED_TRACKED){
+            if(gm.reload == true) self.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            if (gm.track1bool == false){
+                gm.track1bool = true;
+                gm.track1 = self;
+
+            } else if (gm.track2bool == false && gm.track1 != self){
+                gm.track2bool = true;
+                gm.track2 = self;
+            } else{
+                //do nothing
+            }
+        }
+        if (self.CurrentStatus == TrackableBehaviour.Status.LIMITED || self.CurrentStatus == TrackableBehaviour.Status.NO_POSE){
+            if (gm.reload == false){
+                gm.reload = true;
+            }
+            if(gm.track1 == self){
+                gm.track1 = gm.track2;
+                gm.track1bool = gm.track2bool;
+                gm.track2 = null;
+                gm.track2bool = false;
+            } else if (gm.track2 == self){
+                gm.track2bool = false;
+                gm.track2 = null;
+            } else{
+                //do nothing
+            }
+            if (self.gameObject.transform.childCount > 1){
+                foreach(int childIndex in Enumerable.Range(1,self.gameObject.transform.childCount)){
+                    Debug.Log(childIndex);
+                    Destroy(self.gameObject.transform.GetChild(childIndex).gameObject);
+                }            
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class GameManager : MonoBehaviour {
+    
+
+    public bool track1bool, track2bool;
+    public TrackableBehaviour track1, track2;
+    public bool swap = false;
+    public bool reload = true;
+
+    public int threshold = 1;
+
+    // Start is called before the first frame update
+    void Start() {
+        track1bool = track2bool = false;
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        if(track1 != null && track2 != null && DistanceBetweenMarkers(track1, track2) < threshold && swap == true){
+            SwapModel();
+            reload = false;
+        }
+    }
+
+    void SwapModel(){
+        GameObject trackableMarker = track1.gameObject;
+        GameObject otherTrackableGameObject = track2.gameObject;
+        Instantiate(otherTrackableGameObject.gameObject.transform.GetChild(0).gameObject,
+                    trackableMarker.gameObject.transform.GetChild(0).gameObject.transform.position,
+                    otherTrackableGameObject.gameObject.transform.GetChild(0).gameObject.transform.rotation,
+                    trackableMarker.transform);
+        otherTrackableGameObject.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        swap = false;
+        //clone.transform.localRotation = trackableMarker.gameObject.transform.GetChild(0).gameObject.transform.localRotation;
+        //clone.transform.localScale = trackableMarker.gameObject.transform.GetChild(0).gameObject.transform.localScale;
+    }
+
+    double DistanceBetweenMarkers(TrackableBehaviour t1, TrackableBehaviour t2){
+        double dist = Mathf.Min((t1.gameObject.transform.position.x-t2.gameObject.transform.position.x), 
+                                (t1.gameObject.transform.position.y-t2.gameObject.transform.position.y), 
+                                (t1.gameObject.transform.position.z-t2.gameObject.transform.position.z));
+        if (track1bool == track2bool && reload == true){
+            swap = true;
+        }
+        return dist;
+    }
+}
