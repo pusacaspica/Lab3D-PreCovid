6ec7a2b95627bdaf34eb82cf5d99ca72f67d9bae
Horácio Macêdo <horacio97@gmail.com>
2020-01-17 14:14:27 -0300

    atom builder and hijinks
    not all hijinks tho
    
    Now one can create atoms based on real elements, giving the valence value, the atomic number, atomic mass and name. In the near future, fix eletron distribution, make eletrons in different layers, determinate color-coded atoms based on type of element and study how to use Unity UI (or audio) to expose pre-recorded info about different elements.

---
+++ Assets/AtomElementBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class AtomElementBuilder : MonoBehaviour {
+
+    public Element element;
+    public GameObject self;
+    public float eletrosphereRadius = 10;
+    public float eletronSpeed = 100;
+    // Start is called before the first frame update
+    void Start() {
+        GameObject nucleus = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        nucleus.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        nucleus.transform.localScale += new Vector3(element.atomicMass, element.atomicMass, element.atomicMass);
+        self.transform.parent = nucleus.transform;
+        foreach (int i in Enumerable.Range(1, element.valence)){
+            GameObject eletron = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            eletron.transform.position = nucleus.transform.position + new Vector3(0, eletrosphereRadius, 0);
+            eletron.transform.RotateAround(nucleus.transform.localScale, Vector3.right, eletronSpeed*Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Custom/Element.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Element", menuName="Element")]
+public class Element : ScriptableObject {
+
+    public new string name;
+    public int atomicNumber;
+    public int atomicMass;
+    public int valence;
+    public enum atomictype{Hydrogen = 0, AlkaliMetal = 1, AlkalineEarthMetal = 2, TransitionMetal = 3, SemiMetal = 4, NonMetal = 5, NobleGas = 6, Lanthanide = 7, Actinide = 8};
+
+}
